6f4593224cf52ca510c4e3dd5b568cc907c2d9c4
itsmuskanagarwal <agarwal.muskan4444@gmail.com>
2023-02-08 11:44:45 +0530

    Sign up reactive form done

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,48 +1,85 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { StorageService } from '../storage.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
 
   public userdetail!: {
-    "name": string;
-    "contact": string;
-    "email": string;
-    "password": string;
+    name: string;
+    contact: string;
+    email: string;
+    password: string;
   };
 
+  constructor(
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private service: StorageService
+  ) {}
 
-  constructor(private _snackBar: MatSnackBar, private _router:Router, private service:StorageService) {}
+  ngOnInit(): void {}
 
-  signup(username:any, usercontact:any, useremail:any, userpass:any){
-
-    //console.log(this.userdetail);
-    if(username=="" || userpass==""){
-      this._snackBar.open("Invalid Input","OK",{
-        duration:5000
-      });
-    }else{
-    this.userdetail={"name": username,
-    "contact": usercontact,
-    "email":useremail,
-    "password":userpass
-  };
-    this.service.getUserDetails(this.userdetail);
-    this._router.navigate(['login']);
-    this._snackBar.open("Hello "+username+", You are Successfully Registered !!","OK",{
-      duration:5000
+  username: string = "";
+  password: string = "";
+    
+  formData = new FormGroup({
+    username: new FormControl(""),
+    password: new FormControl(""),
+    confirmPassword : new FormControl(""),
+    email : new FormControl(""),
+    contact : new FormControl("")
   });
 
-  }
-}
-  ngOnInit(): void {
+  onClickSubmit(data: any) {
 
-  }
+    console.log(data.userame)
+    console.log(data.password)
+    // console.log(data.contact)
+    // console.log(data.email)
+    
 
+    this.username = data.username;
+    this.password = data.password;
+
+    console.log('Login page: ' + this.username);
+    console.log('Login page: ' + this.password);
+
+    if (this.username == '' || this.password == '') 
+    { 
+      console.log("if is working")
+      this._snackBar.open('Invalid Input', 'OK', {
+        duration: 5000,
+      });
+    } 
+    else
+    { console.log("else is working")
+      this.userdetail = 
+      {
+        name: data.username,
+        contact: data.contact,
+        email: data.email,
+        password: data.password,
+      };
+
+      console.log(this.userdetail)
+
+      this.service.getUserDetails(this.userdetail);
+      this.router.navigate(['login']);
+      this._snackBar.open
+      (
+        'Hello ' + this.username + ', You are Successfully Registered !!',
+        'OK',
+        {
+          duration: 5000,
+        }
+      );
+    }
+  }
 }
